745767a6ade5269f9227e03655aaeeff9a567ece
madicke <madicke.thiane@gmail.com>
2021-08-25 23:36:29 +0200

    Création du formulaire ImageType

---
+++ src/Form/ImageType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Image;
+use App\Entity\Article;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ImageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('lien')
+            ->add('article', EntityType::class, [
+                'label' => 'Article',
+                'choice_label' => 'titre',
+                'class' => Article::class,
+                'expanded' => true,
+                'multiple' => false,
+            ])
+            ->add('Valider', SubmitType::class, [
+                "attr" => [
+                    "class" => "btn btn-success",
+                    "style" => "margin-top : 5px"
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Image::class,
+        ]);
+    }
+}
