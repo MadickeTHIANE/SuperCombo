7f57d936cbb1b75e70c094d80ec954a24c27d3a4
Madické Thiane <madicke.thiane@gmail.com>
2023-04-19 17:39:58 +0200

    Add Reset password button and add locale to route

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -21,7 +21,7 @@ use SymfonyCasts\Bundle\ResetPassword\Exception\ResetPasswordExceptionInterface;
 use SymfonyCasts\Bundle\ResetPassword\ResetPasswordHelperInterface;
 
 /**
- * @Route("/reset-password")
+ * @Route("/{_locale}/reset-password", requirements={"_locale": "en|fr"})
  */
 class ResetPasswordController extends AbstractController
 {
