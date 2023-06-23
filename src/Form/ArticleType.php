e9cf510d8c30123f0ed47ab3a4beaf8e5e598345
Madické Thiane <madicke.thiane@gmail.com>
2023-06-23 16:27:16 +0200

    Add i18n into Video, Slide, Media, Article form and forum

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -4,18 +4,34 @@ namespace App\Form;
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class ArticleType extends AbstractType
 {
+    private $translator;
+    
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('contenu')
+            ->add('titre', TextType::class, [
+                'label' => $this->translator->trans('form.title'),
+                'attr' => [
+                    "maxlength" => "22"
+                    ]
+                ])
+            ->add('contenu', TextType::class,[
+                'label' => $this->translator->trans('form.content'),
+                ])
             ->add('valider', SubmitType::class, [
-                "label" => "Valider",
+                'label' => $this->translator->trans('form.submit'),
                 "attr" => [
                     "class" => "btn btn-success",
                     "style" => "margin-top : 5px"
--- src/Form/SlideType.php
+++ src/Form/SlideType.php
@@ -4,36 +4,61 @@ namespace App\Form;
 use App\Entity\Media;
 use App\Entity\Slide;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class SlideType extends AbstractType
 {
+    private $translator;
+    
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('titre')
-            ->add('extrait')
-            ->add('lien')
-            ->add('button', null, [
-                'label' => "Texte du bouton"
+            ->add('titre', TextType::class, [
+                'label' => $this->translator->trans('form.title'),
+                'attr' => [
+                    "maxlength" => "22"
+                ]
+            ])
+            ->add('extrait', TextType::class, [
+                'label' => $this->translator->trans('form.description'),
+                "attr" => [
+                    "maxlength" => "200", 
+                ]
+            ])
+            ->add('lien', TextType::class,[
+                'label'=> $this->translator->trans('form.link')
+                ])
+            ->add('button', TextType::class, [
+                'label' => $this->translator->trans('form.slide.button-text'),
+                'attr' => [
+                    "maxlength" => "200", 
+                ]
             ])
             ->add('active', null, [
-                'label' => 'Slide principale'
+                'label' => $this->translator->trans('form.slide.main')
             ])
             ->add('textDark')
             ->add('bgDark')
             ->add('media', EntityType::class, [
-                'label' => 'Image',
+                'label' =>  $this->translator->trans('form.slide.image'),
                 'choice_label' => 'title',
                 'class' => Media::class,
                 'expanded' => true,
                 'multiple' => false
             ])
             ->add('Valider', SubmitType::class, [
-                "attr" => [
+                'label' => $this->translator->trans('form.submit'),
+                'attr' => [
                     "class" => "btn btn-success",
                     "style" => "margin-top : 5px"
                 ]
